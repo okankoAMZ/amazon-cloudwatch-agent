71fb0d7a03446c99f310d02d98462de2c441ac28
Okan Kocabalkanli <okanko@amazon.com>
2022-06-21 19:06:00 +0000

    testing github hash fetch

--- integration/test/performance_tracker/mainTest.go
+++ integration/test/performance_tracker/mainTest.go
@@ -1,10 +1,13 @@
 //go:build linux && integration
 // +build linux,integration
 package performance_tracker
+
 import (
+	"os"
 	"testing"
 )
 
 func TestPerformanceTracker(t * testing.T){
 	t.Log("Hello World")
+	t.log("Hash", os.Getenv("SHA"))
 }
